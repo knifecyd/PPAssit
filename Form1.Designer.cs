4e84618a1d3a3d512321dadedac8d7d13b935483
knifecyd allen <knifecyd@qq.com>
2018-03-18 23:46:22 +0800

    111

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace TimerShow
+{
+    partial class ListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tmpbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 10);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(385, 280);
+            this.listBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tmpbox
+            // 
+            this.tmpbox.Location = new System.Drawing.Point(12, 310);
+            this.tmpbox.Name = "tmpbox";
+            this.tmpbox.Size = new System.Drawing.Size(385, 21);
+            this.tmpbox.TabIndex = 54;
+            // 
+            // ListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 488);
+            this.Controls.Add(this.tmpbox);
+            this.Controls.Add(this.listBox1);
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "ListForm";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox tmpbox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerShow
+{
+    public partial class ListForm : Form
+    {
+
+        private Dictionary<String, String> dic;
+        public TimerShowDlg t;
+        public ListForm(Dictionary<String, String> dic1)
+        {
+            InitializeComponent();
+            dic = new Dictionary<string, string>();
+            //t = ((TimerShowDlg)this.Owner);
+            this.dic = dic1;
+
+        }
+        public ListForm( )
+        {
+            InitializeComponent();
+            dic = new Dictionary<string, string>();
+           // t = ((TimerShowDlg)this.Owner);
+            //this.dic = dic1;
+
+        }
+
+        public void  setTimePriceList( Dictionary<String,String> d) 
+        {
+            this.dic = d;
+
+        }
+        public void showDic()
+        {
+            this.listBox1.Items.Clear();
+            
+            foreach (KeyValuePair<String, String> pair in dic)
+            {
+               // if (!this.listBox1.Items.Contains(pair))
+                {
+                    this.listBox1.Items.Add(pair.Key + "|" + pair.Value);
+                }
+
+                this.tmpbox.Text = pair.Key + " | " + pair.Value;
+            }
+
+            this.listBox1.Show();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.showDic();
+        }
+    }
+}
